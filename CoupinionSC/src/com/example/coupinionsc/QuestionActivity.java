8e24c57bf6163c8db359e2ea66e8acd90423a860
Mitsukizzy <ibenaven@usc.edu>
2013-10-12 12:38:51 -0700

    team first commit

---
+++ CoupinionSC/src/com/example/coupinionsc/QuestionActivity.java
@@ -0,0 +1,22 @@
+package com.example.coupinionsc;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class QuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_question);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.question, menu);
+		return true;
+	}
+
+}
